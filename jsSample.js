b967539b183b6dfc429f5bb57249676984271c3e
Jsvenzon <jsvenzon.personal@gmail.com>
2023-03-18 18:32:03 +0800

    For Testing Only

---
+++ jsSample.js
@@ -0,0 +1,4 @@
+console.log("--------------------------------------------------------");
+console.log("I am greatly blessed, deeply loved, and highly favored");
+console.log("Praise the Lord");
+console.log("---------------------------------------------------------");
